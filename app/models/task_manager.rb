7223c048fdea1dcb13276ee7366eb1d5d459043d
Ben Pepper <bfpepper87@gmail.com>
2016-08-16 07:32:28 -0600

    trying to add style

--- app/models/task_manager.rb
+++ app/models/task_manager.rb
@@ -12,7 +12,7 @@ require 'yaml/store'
      database.transaction do
        database['tasks'] ||= []
        database['total'] ||= 0
-       database['total'] = 1
+       database['total'] += 1
        database['tasks'] << { "id" => database['total'], "title" => task[:title], "description" => task[:description] }
      end
    end
